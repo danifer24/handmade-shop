2fa9c993e84271d4f4d8448159f8eccbf983ac8f
danifer24 <danielvf24@gmail.com>
2025-11-22 11:28:17 +0100

    Update dependencies and improve the Header component with navigation and new icons

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,20 +1,42 @@
-import { Box, Flex, Text, Button } from "@chakra-ui/react";
+import { Box, Flex, Text, Button, HStack, Icon, Heading } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { IoCartOutline } from "react-icons/io5";
+
 
 export default function Header() {
   return (
-    <Box bg="brand.500" color="white" px={6} py={4} boxShadow="md">
+    <Box as="header" bg="brand.500" color="white" px={6} py={4} boxShadow="md">
       <Flex justify="space-between" align="center">
-        <Text fontSize="xl" fontWeight="bold">
-          Handmade Shop
-        </Text>
+        <Heading size="md"><NavLink to="/">Handmade Shop</NavLink></Heading>
 
-        <Flex gap={4}>
-          <Button variant="ghost" color="white">Productos</Button>
-          <Button variant="ghost" color="white">Sobre Nosotros</Button>
-          <Button variant="outline" color="white" borderColor="white">
-            Login
+        <HStack spacing={3} display={{ md: "flex" }} pr={10}>
+          <Button variant={"ghost"} p={0}>
+            <Icon
+              as={IoIosSearch}
+              boxSize={7}
+              _hover={{ transform: "scale(1.15)" }}
+              transition="0.15s"
+            />
           </Button>
-        </Flex>
+          <Button as={NavLink} to={"/login"} variant={"ghost"} p={0}>
+            <Icon
+              as={MdOutlinePersonOutline}
+              boxSize={7}
+              _hover={{ transform: "scale(1.15)" }}
+              transition="0.15s"
+            />
+          </Button>
+          <Button as={NavLink} to={"/cart"} variant={"ghost"} p={0}>
+            <Icon
+              as={IoCartOutline}
+              boxSize={7}
+              _hover={{ transform: "scale(1.15)" }}
+              transition="0.15s"
+            />
+          </Button>
+        </HStack>
       </Flex>
     </Box>
   );
